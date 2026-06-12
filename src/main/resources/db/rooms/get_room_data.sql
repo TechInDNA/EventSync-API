18c6582ff0f2fb1af785947c89b72ed4a67b675c
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-06-12 15:06:02 +0300

    feat: add seed data for GET /rooms/id endpoint

---
+++ src/main/resources/db/rooms/get_room_data.sql
@@ -0,0 +1,4 @@
+-- Seed: room pour tester GET /rooms/id (UUID fixe généré par uuidgen)
+INSERT INTO eventsync_app."room" (id, name)
+VALUES ('134873ba-e426-4e1b-8943-a8ee4d0951b7', 'Room de test GET by ID')
+ON CONFLICT (name) DO NOTHING;
