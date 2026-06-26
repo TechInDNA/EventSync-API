74a8e46bc88688376b80e15874547b8bfca950a3
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-06-26 15:54:54 +0300

    Revert "fix: add missing unique constraint on session_speaker(session_id, speaker_id)"
    This reverts commit 99188aa2d81ef9f9992b722476218012de95094d.

--- src/main/resources/db/sessions/session_speaker_schema.sql
+++ src/main/resources/db/sessions/session_speaker_schema.sql
@@ -1,10 +1,9 @@
 
 CREATE TABLE IF NOT EXISTS eventsync_app.session_speaker (
-    id UUID DEFAULT gen_random_uuid(),
     session_id  UUID                    NOT NULL  REFERENCES eventsync_app."session"(id) ON DELETE CASCADE,
     speaker_id  UUID                    NOT NULL  REFERENCES eventsync_app."user"(id) ON DELETE CASCADE,
     start_time  TIMESTAMPTZ  NOT NULL,
     end_time    TIMESTAMPTZ  NOT NULL,
-    UNIQUE (session_id, speaker_id),
+    PRIMARY KEY (session_id, speaker_id),
     UNIQUE (start_time, end_time)
 );
