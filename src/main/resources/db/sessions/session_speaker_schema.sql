99188aa2d81ef9f9992b722476218012de95094d
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-06-26 15:52:50 +0300

    fix: add missing unique constraint on session_speaker(session_id, speaker_id)

--- src/main/resources/db/sessions/session_speaker_schema.sql
+++ src/main/resources/db/sessions/session_speaker_schema.sql
@@ -1,9 +1,10 @@
 
 CREATE TABLE IF NOT EXISTS eventsync_app.session_speaker (
+    id UUID DEFAULT gen_random_uuid(),
     session_id  UUID                    NOT NULL  REFERENCES eventsync_app."session"(id) ON DELETE CASCADE,
     speaker_id  UUID                    NOT NULL  REFERENCES eventsync_app."user"(id) ON DELETE CASCADE,
     start_time  TIMESTAMPTZ  NOT NULL,
     end_time    TIMESTAMPTZ  NOT NULL,
-    PRIMARY KEY (session_id, speaker_id),
+    UNIQUE (session_id, speaker_id),
     UNIQUE (start_time, end_time)
 );
