434a4696135260da7bc840a1d42774baf15b8dab
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-06-26 15:33:18 +0300

    fix: use timestamptz for session_speaker start/end time and add unique constraint

--- src/main/resources/db/sessions/session_speaker_schema.sql
+++ src/main/resources/db/sessions/session_speaker_schema.sql
@@ -2,7 +2,8 @@
 CREATE TABLE IF NOT EXISTS eventsync_app.session_speaker (
     session_id  UUID                    NOT NULL  REFERENCES eventsync_app."session"(id) ON DELETE CASCADE,
     speaker_id  UUID                    NOT NULL  REFERENCES eventsync_app."user"(id) ON DELETE CASCADE,
-    start_time  TIME WITH TIME ZONE     NOT NULL,
-    end_time    TIME WITH TIME ZONE     NOT NULL,
-    PRIMARY KEY (session_id, speaker_id)
+    start_time  TIMESTAMPTZ  NOT NULL,
+    end_time    TIMESTAMPTZ  NOT NULL,
+    PRIMARY KEY (session_id, speaker_id),
+    UNIQUE (start_time, end_time)
 );
